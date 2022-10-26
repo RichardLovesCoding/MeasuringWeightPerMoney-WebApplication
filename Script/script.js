bf5e7a8db8fc88fbef0ad79c55137c03d30e2f55
Richard Xaikia <104146031+RichardLovesCoding@users.noreply.github.com>
2022-10-26 21:23:28 +0530

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -2,14 +2,7 @@ let pricePerOnekg = document.querySelector('#massPrice')
 let yourTotalPrice = document.querySelector('#yourPrice')
 let finalWeightAmount = document.querySelector('#finalGmAmount')
 
-// const maintenance = () => {
-//   if(pricePerOnekg == 0 && yourTotalPrice == 0){
-//     finalWeightAmount.innerText = 0.00gm
-//   }
-//   else if(pricePerOnekg == 0){
-//      finalWeightAmount.innerText = 0.00gm
-//   }
-// }
+
 
 const initialWeight = () => {
   finalWeightAmount.innerText = 1000 + ' gm'
@@ -22,8 +15,3 @@ const calculateActualWeight = () => {
   finalWeightAmount.innerText = `${line2.toFixed(2)}gm`
 }
 
-/* the syntex:
-let line1 = 1000 / ppokgintoNumbers
-let line2 = ytpintoNumbers * line1
-finalWeightAmount.innerText = `${line2}gm`
-*/
